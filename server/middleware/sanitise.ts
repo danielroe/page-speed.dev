7477e27f2dc8ad76cc219973ce93dd230846c730
Daniel Roe <daniel@roe.dev>
2024-02-01 20:36:55 +0000

    fix: exempt og images

--- server/middleware/sanitise.ts
+++ server/middleware/sanitise.ts
@@ -1,7 +1,7 @@
 import { withoutLeadingSlash } from 'ufo'
 
 export default defineEventHandler(async event => {
-  if (event.path.startsWith('/api')) { return }
+  if (event.path.startsWith('/api') || event.path.startsWith('/__og-image__')) { return }
 
   const path = '/' + withoutLeadingSlash(event.path).toLowerCase().replace(/(\/|\?).*$/, '').trim()
   if (event.path !== path) {
