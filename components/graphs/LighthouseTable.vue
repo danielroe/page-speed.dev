e7071afb6a1dfd84412e87da072bd47b0e076220
Daniel Roe <daniel@roe.dev>
2024-02-08 21:09:07 +0000

    fix: improve stability of loading screen

--- components/graphs/LighthouseTable.vue
+++ components/graphs/LighthouseTable.vue
@@ -17,8 +17,9 @@ const circumference = normalizedRadius * 2 * Math.PI
 <template>
   <div class="flex flex-row flex-wrap gap-4 lg:flex-row justify-around w-full border border-green-700 border-2 rounded-lg p-4">
     <span v-for="[metric, caption] of ([[performance, 'performance'], [accessibility, 'accessibility'], [bestPractices, 'best practices'], [seo, 'SEO']] as const)" class="flex flex-row gap-2" :class="{ 'flex-[40%] md:flex-auto': !row }">
-      <svg v-if="loading" :height="radius * 2" :width="radius * 2" class="animate-spin">
-        <circle
+      <div v-if="loading"  class="min-w-8 md:min-w-0 mr-1 flex justify-end">
+        <svg :height="radius * 2" :width="radius * 2" class="animate-spin">
+          <circle
           stroke="#6b7280"
           fill="transparent"
           class="transform-origin-center"
@@ -28,9 +29,10 @@ const circumference = normalizedRadius * 2 * Math.PI
           :r="normalizedRadius"
           :cx="radius"
           :cy="radius"
-        />
-      </svg>
-      <span v-else class="font-bold tabular-nums min-w-[2.25rem] md:min-w-0 text-right mr-1">{{ loading ? undefined : metric?.toFixed(0) }}</span>
+          />
+        </svg>
+      </div>
+      <span v-else class="font-bold tabular-nums min-w-8 md:min-w-0 text-right mr-1">{{ loading ? undefined : metric?.toFixed(0) }}</span>
       <span>
         {{ caption }}
       </span>
