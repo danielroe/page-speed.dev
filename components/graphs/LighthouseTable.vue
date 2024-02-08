4fee9a9432d18bfebbc62003944d4216716a54c2
Daniel Roe <daniel@roe.dev>
2024-02-08 21:10:04 +0000

    fix: improve stability on desktop too

--- components/graphs/LighthouseTable.vue
+++ components/graphs/LighthouseTable.vue
@@ -17,7 +17,7 @@ const circumference = normalizedRadius * 2 * Math.PI
 <template>
   <div class="flex flex-row flex-wrap gap-4 lg:flex-row justify-around w-full border border-green-700 border-2 rounded-lg p-4">
     <span v-for="[metric, caption] of ([[performance, 'performance'], [accessibility, 'accessibility'], [bestPractices, 'best practices'], [seo, 'SEO']] as const)" class="flex flex-row gap-2" :class="{ 'flex-[40%] md:flex-auto': !row }">
-      <div v-if="loading"  class="min-w-8 md:min-w-0 mr-1 flex justify-end">
+      <div v-if="loading"  class="min-w-8 mr-1 flex justify-end">
         <svg :height="radius * 2" :width="radius * 2" class="animate-spin">
           <circle
           stroke="#6b7280"
@@ -32,7 +32,7 @@ const circumference = normalizedRadius * 2 * Math.PI
           />
         </svg>
       </div>
-      <span v-else class="font-bold tabular-nums min-w-8 md:min-w-0 text-right mr-1">{{ loading ? undefined : metric?.toFixed(0) }}</span>
+      <span v-else class="font-bold tabular-nums min-w-8 text-right mr-1">{{ loading ? undefined : metric?.toFixed(0) }}</span>
       <span>
         {{ caption }}
       </span>
