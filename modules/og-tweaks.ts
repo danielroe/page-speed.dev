8710be0af472051d181e59fc38044671125b7046
Daniel Roe <daniel@roe.dev>
2024-02-09 11:32:06 +0000

    perf: don't include community og templates in build

---
+++ modules/og-tweaks.ts
@@ -0,0 +1,19 @@
+import { defineNuxtModule, useNuxt } from 'nuxt/kit'
+
+export default defineNuxtModule({
+  meta: {
+    name: 'og-tweaks',
+  },
+  setup () {
+    const nuxt = useNuxt()
+
+    nuxt.hook('modules:done', () => {
+      nuxt.hook('components:dirs', (dirs) => {
+        const ogCommunityTemplates = dirs.findIndex(i => typeof i !== 'string' && i.path.includes('components/Templates/Community'))
+        if (ogCommunityTemplates !== -1) {
+          dirs.splice(ogCommunityTemplates, 1)
+        }
+      })
+    })
+  },
+})
