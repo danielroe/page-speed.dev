69ab58772c5785c4aab80e85cf0af210190c37e9
Daniel Roe <daniel@roe.dev>
2024-02-10 00:03:43 +0000

    fix: try url autocomplete

--- components/TheDomainForm.vue
+++ components/TheDomainForm.vue
@@ -32,7 +32,7 @@ function navigateToNewDomain () {
   <form v-else class="flex flex-col gap-4 overflow-hidden" @submit.prevent="navigateToNewDomain">
     <input ref="input" v-model="newDomain" name="domain" type="text"
       class="md:-mt-1 rounded-none py-0 bg-transparent outline-none border-b-2 border-b-solid border-transparent focus:border-green-500 underline-dashed"
-      autofocus autocomplete="on" inputmode="url" autocapitalize="none" placeholder="enter a domain" required />
+      autofocus autocomplete="url" inputmode="url" autocapitalize="none" placeholder="enter a domain" required />
     <button type="submit"
       class="bg-green-400 text-black hover: hover:bg-white focus:bg-white active:bg-white text-xl md:text-2xl py-2 px-6 md:self-start">see
       results</button>
