7b968a61dfee9a684f975eee0300abdb840902b8
Daniel Roe <daniel@roe.dev>
2024-02-10 00:13:53 +0000

    perf: use native formdata rather than tracking input value

--- components/TheDomainForm.vue
+++ components/TheDomainForm.vue
@@ -4,24 +4,24 @@ import { withoutLeadingSlash, parseURL } from 'ufo'
 const domain = defineModel('domain', { type: String, default: '' })
 const editing = defineModel('editing', { type: Boolean })
 
-const newDomain = ref('')
 const input = ref<HTMLInputElement>()
 
 function enableEditing () {
-  newDomain.value = domain.value
   editing.value = true
   watch(input, (input) => {
     if (input) {
+      input.value = domain.value
       input.focus()
-      input.setSelectionRange(0, newDomain.value.length)
+      input.setSelectionRange(0, input.value.length)
     }
   }, { once: true })
 }
 
-function navigateToNewDomain () {
-  if (!newDomain.value) { return }
+function navigateToNewDomain (event: Event) {
+  const newDomain = new FormData(event.target as HTMLFormElement).get('domain') as string
+  if (!newDomain) { return }
 
-  const host = parseURL(newDomain.value).host || newDomain.value
+  const host = parseURL(newDomain).host || newDomain
   editing.value = false
   return navigateTo('/' + withoutLeadingSlash(host).toLowerCase().replace(/(\/|\?).*$/, '').trim())
 }
@@ -30,7 +30,7 @@ function navigateToNewDomain () {
 <template>
   <button v-if="domain && !editing" class="bg-transparent" @click="enableEditing">{{ domain }}</button>
   <form v-else class="flex flex-col gap-4 overflow-hidden" @submit.prevent="navigateToNewDomain">
-    <input ref="input" v-model="newDomain" name="domain" type="text"
+    <input ref="input" name="domain" type="text"
       class="md:-mt-1 rounded-none py-0 bg-transparent outline-none border-b-2 border-b-solid border-transparent focus:border-green-500 underline-dashed"
       autofocus autocomplete="url" inputmode="url" autocapitalize="none" placeholder="enter a domain" required />
     <button type="submit"
