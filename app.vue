19fad992c454440324f6b8847d6ba22a8abab863
Jamie Slome <jamieizakslome@gmail.com>
2024-02-11 19:08:54 +0000

    chore: only display confetti when both cwv and lighthouse pass and all score 100

--- app.vue
+++ app.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="md:pl-[5vw] font-sans text-white min-h-screen min-w-screen flex flex-col items-start justify-start">
-    <div v-if="crux?.cwv || [lighthouse?.performance, lighthouse?.seo, lighthouse?.accessibility, lighthouse?.bestPractices].every(v => v === 100)" v-confetti /> {{ crux }} {{ lighthouse }}
+    <div v-if="crux?.cwv && [lighthouse?.performance, lighthouse?.seo, lighthouse?.accessibility, lighthouse?.bestPractices].every(v => v === 100)" v-confetti />
     <div class="flex flex-col justify-start mt-4 sm:mt-8 md:my-12 p-4 gap-8 md:gap-12 flex-grow max-w-full">
       <h1 class="flex flex-row gap-4 text-white text-3xl md:text-5xl">
         <span class="text-green-400">&raquo;</span>
