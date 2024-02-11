2de9d11b97ba37db13cd2fa1e7cd328690f3f94f
Daniel Roe <daniel@roe.dev>
2024-02-11 23:11:52 +0000

    fix: remove `purgecss`

--- modules/inline-css.ts
+++ modules/inline-css.ts
@@ -1,7 +1,4 @@
 import { addVitePlugin, createResolver, defineNuxtModule, useNuxt } from 'nuxt/kit'
-import { defu } from 'defu'
-import purgehtml from 'purgecss-from-html'
-import { PurgeCSS } from 'purgecss'
 
 export default defineNuxtModule({
   meta: {
@@ -12,7 +9,6 @@ export default defineNuxtModule({
     const resolver = createResolver(import.meta.url)
 
     let css: string
-    let cssFilename: string
 
     addVitePlugin({
       name: 'entry-css',
@@ -20,36 +16,12 @@ export default defineNuxtModule({
         for (const name in bundle) {
           const asset = bundle[name]
           if (asset.type === 'asset' && asset.fileName.match(/entry.*\.css$/)) {
-            cssFilename = asset.fileName
             css = asset.source.toString()
-            asset.source = ''
             return
           }
         }
       }
-    })
-
-    // Collect example routes
-    nuxt.options.nitro.prerender = defu(nuxt.options.nitro.prerender, {
-      routes: ['/roe.dev', '/atinux.com']
-    })
-    const htmlRoutes: string[] = []
-    nuxt.hook('nitro:init', nitro => {
-      nitro.hooks.hook('prerender:generate', async route => {
-        if (!route.fileName?.endsWith('.html') || !route.contents) return
-        htmlRoutes.push(route.contents)
-        route.skip = true
-      })
-    })
-
-    let extractedCSS = ''
-    nuxt.hook('nitro:build:public-assets', async () => {
-      [{ css: extractedCSS }] = await new PurgeCSS().purge({
-        content: htmlRoutes.map(html => ({ raw: html, extension: 'html' })),
-        css: [{ raw: css }],
-        extractors: [{ extensions: ['html'], extractor: purgehtml }],
-      })
-    })
+    }, { server: false })
 
     nuxt.hook('build:manifest', manifest => {
       for (const key in manifest) {
@@ -61,7 +33,7 @@ export default defineNuxtModule({
 
     // Add virtual entry to Nitro
     nuxt.options.nitro.virtual ||= {}
-    nuxt.options.nitro.virtual['#inline-styles'] = () => `export default ${JSON.stringify(`<style>${extractedCSS}</style>`)}`
+    nuxt.options.nitro.virtual['#inline-styles'] = () => `export default ${JSON.stringify(`<style>${css}</style>`)}`
 
     nuxt.options.nitro.plugins ||= []
     nuxt.options.nitro.plugins.push(resolver.resolve('./inline-css/nitro-plugin'))
