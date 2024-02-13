c9c9061622137ecb04c2b7669b38ac5166137503
Jamie Slome <jamieizakslome@gmail.com>
2024-02-13 13:46:57 +0000

    feat: add confetti for CWV pass + 100/100/100/100 scores (#18)

--- app.vue
+++ app.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="md:pl-[5vw] font-sans text-white min-h-screen min-w-screen flex flex-col items-start justify-start">
+    <div v-if="showConfetti" v-confetti />
     <div class="flex flex-col justify-start mt-4 sm:mt-8 md:my-12 p-4 gap-8 md:gap-12 flex-grow max-w-full">
       <div class="flex flex-row gap-4 text-white text-3xl md:text-5xl">
         <span class="text-green-400">&raquo;</span>
@@ -64,6 +65,7 @@
 <script lang="ts" setup>
 import '@unocss/reset/tailwind-compat.css'
 import { joinURL, withoutLeadingSlash } from 'ufo'
+import { vConfetti } from '@neoconfetti/vue'
 
 const route = useRoute()
 const domain = computed(() => withoutLeadingSlash(route.path).toLowerCase().replace(/(\/|\?).*$/, '').trim())
@@ -80,6 +82,15 @@ const { data: lighthouse, status: lighthouseStatus, refresh: lighthouseRefresh }
   watch: [hasValidDomain],
   immediate: hasValidDomain.value,
   server: false,
+})
+
+const keys = ['performance', 'accessibility', 'bestPractices', 'seo'] as const
+const showConfetti = computed(() => {
+  // CWV fail, but ignore if there is now CrUX data
+  if (cruxStatus.value !== 'pending' && crux.value && !crux.value.cwv) {
+    return false
+  }
+  return lighthouse.value && keys.every(key => lighthouse.value![key] === 100)
 })
 
 useFavicon(() => lighthouseStatus.value !== 'pending' && !!hasValidDomain.value && (lighthouse.value ? lighthouse.value.performance : 100))
