f08b8a41558bea2838ea49df3a7c89f7d4975fdf
Daniel Roe <daniel@roe.dev>
2024-02-13 13:59:46 +0000

    fix: handle backslashes

--- utils/validation.ts
+++ utils/validation.ts
@@ -5,5 +5,5 @@ const VALID_DOMAIN_RE = /^[a-zA-Z0-9.\-]+\.[a-zA-Z]{2,}$/
 export const isValidDomain = (domain: string) => VALID_DOMAIN_RE.test(domain)
 
 export function sanitizeDomain(domain: string) {
-  return withoutLeadingSlash(domain).toLowerCase().replace(/(\/|\?).*$/, '').trim()
+  return withoutLeadingSlash(domain).toLowerCase().replace(/\\/g, '').replace(/[?/].*$/, '').trim()
 }
