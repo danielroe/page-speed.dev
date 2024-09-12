29ad8caeb23c7073124947b53c55234f41a4547f
Daniel Roe <daniel@roe.dev>
2024-09-12 22:53:31 +0100

    chore: upgrade dependencies

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  compatibilityDate: '2024-09-12',
   devtools: { enabled: true },
   components: ['~/components/graphs', '~/components'],
   features: {
