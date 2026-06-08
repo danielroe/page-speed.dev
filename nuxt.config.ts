b77e7406daa552b3f61ac1cd92e5e58d29f6831a
Daniel Roe <daniel@roe.dev>
2026-06-08 17:34:52 +0100

    revert: drop build-time api token wiring

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -27,7 +27,7 @@ export default defineNuxtConfig({
   },
   runtimeConfig: {
     google: {
-      apiToken: process.env.NUXT_GOOGLE_API_TOKEN || '',
+      apiToken: '',
     },
   },
   routeRules: {
